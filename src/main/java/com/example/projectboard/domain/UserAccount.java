93aa001fc6c35c235a175d2377d490f01f743670
tg <tg1218.kim@google.com>
2023-12-24 21:35:25 +0900

    유저 도메인 추가 로그인 페이지 추가

---
+++ src/main/java/com/example/projectboard/domain/UserAccount.java
@@ -0,0 +1,36 @@
+package com.example.projectboard.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Table(indexes = {
+        @Index(columnList = "email", unique = true),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserAccount extends AuditingFields {
+    @Id
+    @Column(length = 50)
+    private String userId;
+
+    @Setter
+    @Column(nullable = false)
+    private String userPassword;
+
+    @Setter
+    @Column(length = 100)
+    private String email;
+
+    @Setter
+    @Column(length = 100)
+    private String nickname;
+
+    @Setter
+    private String memo;
+}
